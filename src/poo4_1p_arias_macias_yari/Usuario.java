6593202bf5af9222cc8f98fa4fbd75229b558699
StevenYari <Steayari@espol.edu.ec>
2024-06-20 00:00:43 -0500

    Create Usuario.java

---
+++ src/poo4_1p_arias_macias_yari/Usuario.java
@@ -0,0 +1,24 @@
+public class usuario {
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private char rol;
+public usuario(String nombre,String apellido,String correo,char rol){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.correo=correo;
+    this.rol=rol;
+    
+}
+public String getNombre(){return this.nombre;}
+public void setNombre(String nombre){this.nombre=nombre;}
+public void setApellido(String apellido){this.apellido=apellido;}
+public String getApellido(){return this.apellido;}
+public String getCorreo(){return this.correo;}
+public void setCorreo(String correo){this.correo=correo;}
+public char getRol(){return this.rol;}
+public void setRol(char rol){this.rol=rol;}
+
+public void someterArticulo(Articulo articulo){}
+public void GenerarCorreo(){}
+}
