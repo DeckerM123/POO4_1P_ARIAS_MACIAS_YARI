db59709d8aa10b07a98808068528fee2698ce3e9
StevenYari <Steayari@espol.edu.ec>
2024-06-20 00:14:46 -0500

    Update Revisor.java

--- src/poo4_1p_arias_macias_yari/Revisor.java
+++ src/poo4_1p_arias_macias_yari/Revisor.java
@@ -1,37 +1,37 @@
 public class Revisor extends Usuario{
      private String especialidad;
      private String contrasena;
-     private int numArticulos;
+     private int numArticulo;
 
     // Constructores
-    public Revisor(String nombre, String apellido, String correo, String user, String especialidad, String contrasena,int numArticulos){
+    public Revisor(String nombre, String apellido, String correo, String user, String especialidad, String contrasena,int numArticulo){
         super(nombre, apellido, correo, user, "R");
         this.especialidad = especialidad;
         this.contrasena=contrasena;
-        this.numArticulos=numArticulos;
+        this.numArticulo=numArticulo;
     }
 
     // Getters y Setters
-    public String getespecialidad() {
+    public String getEspecialidad() {
         return especialidad;
     }
 
-    public void setespecialidad(String especialidad) {
+    public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
     }
     
-     public String getcontrasena() {
+     public String getContrasena() {
         return contrasena;
     }
 
-    public void setcontrasena(String contrasena) {
+    public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
     }
-    public int getnumArticulos() {
-        return numArticulos;
+    public int getNumArticulo() {
+        return numArticulo;
     }
 
-    public void setnumArticulos(int numArticulos) {
-        this.numArticulos = numArticulos;
+    public void setNumArticulos(int numArticulo) {
+        this.numArticulo = numArticulo;
     }
-
+}
