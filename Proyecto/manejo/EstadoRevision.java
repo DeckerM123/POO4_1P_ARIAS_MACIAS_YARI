d4120d3a6b60ed8a77edaf2a9f89560b7c20eece
StevenYari <Steayari@espol.edu.ec>
2024-07-11 22:52:05 -0500

    Update EstadoRevision.java

--- Proyecto/manejo/EstadoRevision.java
+++ Proyecto/manejo/EstadoRevision.java
@@ -5,29 +5,24 @@ public enum EstadoRevision {
      * Revisión de un artículo pendiente.
      */
     PENDIENTE("Pendiente"),
-
     /**
      * Revisión de un artículo realizada por el revisor asignado.
      */
     REVISADO("Revisado"),
-
     /**
      * Revisión de un artículo analizada por un editor.
      */
     ANALIZADO("Analizado"),
-
     /**
      * Revisión de un artículo verificada por un editor, que decidió si se publica o no.
      */
     VERIFICADO("Verificado");
-
     private final String descripcion;
-
+    
     private EstadoRevision(String descripcion) {
         this.descripcion = descripcion;
     }
-
-    public String getDescripcion() {
+    public String getDescripciom() {
         return descripcion;
     }
 }
